f8eb7034747f6a6c44bd74e0e284b508d6b42789
brinda behal <brindab50@gmail.com>
2019-05-08 00:05:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, name)
  puts string 
 end
 def now_serving(katz_deli)
-  puts katz_deli
+  puts "Currently serving #{name}"
   if katz_deli == 0 
     puts "There is nobody waiting to be served!"
   else 
